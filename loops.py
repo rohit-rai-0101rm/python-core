1bea99e0899117e7fec22798d826556f6eb6777e
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2026-06-22 08:19:39 +0530

    add loops practised

--- loops.py
+++ loops.py
@@ -8,9 +8,173 @@
 
 # print("count",count)
 
-count=5
+# count=5
 
-while count >=0:
-    print("i",count)
-    count-=1
+# while count >=0:
+#     print("i",count)
+#     count-=1
+
+# 1 to 100
+
+# i=1
+
+# while(i<=100):
+#     print("i",i)
+#     i+=1
+
+
+
+# 100 to 1
+
+# 100 to 1
+# i=100
+
+# while i>=1:
+#     print("i",i)
+#     i-=1
+
+
+# table of any number
+# n=int(input("Enter your fav number "))
+# i=1
+
+# while i<=10:
+#     print(f"{n}*{i}={n*i}")
+#     i+=1
+
+
+# nums=[1,4,9,16,25,36,49,64,81,100]
+
+# i=0
+# while i<len(nums):
+#     print (f"{i}th index number is ",nums[i])
+#     i+=1
+
+
+# tup=(2,3,5)
+
+# print("tuple",type(tup))
+# i=0
+
+# search=5
+# while i<len(tup):
+#     # print("i",tup[i])
+#     if tup[i]==search:
+#         print("search term found",i)
+#         continue
+#     else :
+#         print("finding")
+    
+    
+#     i+=1
+
+
+
+
+
+
+# i=0
+
+# while i<=10:
+#     if(i%2!=0):
+#         i+=1
+#         continue
+#     print("i",i)
+#     i+=1
+
+
+
+
+
+# nums=[1,4,9,16,25,36,49,64,"Rohit","Besan"]
+
+# tup=(2,6,7,9,3,6,77)
+# for el in tup:
+#     print(el)
+# else:
+#     print("END")
+
+
+
+# tup=(1,4,9,16,25,36,64,81,100)
+
+# searchTerm=9
+# idx=0
+# for el in tup:
+#     if el==searchTerm:
+#         print (f"{searchTerm} found at {idx}")
+#         break
+#     idx+=1
+
+
+
+
+# range
+
+
+# for el in range(5):
+#     print (el)
+
+# for el in range (2,10):
+#     print (el)
+
+# print ("step count")
+# for el in range (2,10,2):
+#     print (el)
+
+
+
+
+# print ("100 to 1 using range count")
+# for el in range (100,0,-1):
+#     print (el)
+
+
+# n=int (input("enter number "))
+
+
+# for i in range(1,11):
+#     print (n*i)
+
+
+# print ("pss check")
+
+
+# for el in range (9):
+#     pass
+
+
+
+
+# n=int (input("enter number "))
+# i=1
+# sum=0
+# while i<=n:
+#     sum+=i
+#     i+=1
+
+
+# print("sum",sum)
+
+
+n=int (input("enter number "))
+fact=1
+prod=0
+while n>0:
+    fact*=n
+    n-=1
+
+
+print("fact",fact)
+
+
+
+
+
+
+
+
+
+
+
 
